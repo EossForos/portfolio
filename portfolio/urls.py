6140f698fcb236df398798cb19c879619796b8ca
EossForos <volosonopulos@gmail.com>
2020-03-06 18:59:19 +0200

    Bootstrap theme

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -16,9 +16,11 @@ Including another URLconf
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
+import jobs.views
 
 from portfolio import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', jobs.views.home, name="home"),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
