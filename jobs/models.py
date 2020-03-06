898e69af366294d54db043c1f6d9d438094cf2d2
EossForos <volosonopulos@gmail.com>
2020-03-06 22:02:29 +0200

    apperance and testing

--- jobs/models.py
+++ jobs/models.py
@@ -4,3 +4,6 @@ class Job(models.Model):
     """My jobs"""
     image = models.ImageField(upload_to='Job_images/')
     summary = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.summary
